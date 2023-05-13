2f1c4154b9004ee5baa4fda306ff5ea01cd3737f
Matt Payne <Payne@MattPayne.org>
2023-05-12 19:48:13 -0500

    yarn redwood generate page home /

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -12,6 +12,7 @@ import { Router, Route } from '@redwoodjs/router'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/" page={HomePage} name="home" />
       <Route path="/mattpayne1" page={Mattpayne1Page} name="mattpayne1" />
       <Route notfound page={NotFoundPage} />
     </Router>
